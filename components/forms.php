0b1e54e82d87801897cd2ad3f887dd9f0b5af022
Prox-C <amadeusgravoso@gmail.com>
2025-03-06 16:43:58 +0800

    data handling and validation

--- components/forms.php
+++ components/forms.php
@@ -101,18 +101,18 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body p-5 pt-0">
-        <form>
+        <form method="POST" action="../admin-panel/clients.php">
           <label class="form-label">Personal Information</label>
           <div class="row">
             <div class="col-md-6">
               <div class="form-floating mb-3">
-                <input type="email" class="form-control rounded-3" id="floatingInput"placeholder="First name" required>
+                <input name="fname" type="text" class="form-control rounded-3" id="floatingInput"placeholder="First name" required>
                 <label for="floatingInput">First name</label>
               </div>
             </div>
             <div class="col-md-6">
               <div class="form-floating mb-3">
-                <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com" required>
+                <input name="lname" type="text" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com" required>
                 <label for="floatingInput">Last name</label>
               </div>
             </div>
@@ -120,33 +120,33 @@
           <div class="row">
             <div class="col-md-6">
               <div class="form-floating mb-3">
-                <input type="date" class="form-control rounded-3" id="pickupDate" name="rentStart" required>
+                <input name="bday" type="date" class="form-control rounded-3" id="pickupDate" required>
                 <label for="pickupDate" class="text-muted-subtle">Date of Birth</label>
               </div>
             </div>
             <div class="col-md-6 pb-3">
-              <select class="form-select form-select-md mb-3 rounded-3 h-100 m-0" aria-label=".form-select-lg example">
-                <option class="d-none" selected>Sex<span class="text-muted-subtle"></span></option>
-                <option value="1">Male</option>
-                <option value="2">Female</option>
-                <option value="3">Prefer not to say</option>
+              <select name="sex" class="form-select form-select-md mb-3 rounded-3 h-100 m-0" aria-label=".form-select-lg example" required>
+                <option value="" class="d-none" disabled selected>Sex<span class="text-muted-subtle"></span></option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Prefer not to say">Prefer not to say</option>
               </select>
             </div>
           </div>
           <label for="" class="form-label">Contact Details</label>
           <div class="form-floating mb-3">
-            <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="Email" required>
+            <input name="email" type="email" class="form-control rounded-3" id="floatingInput" placeholder="Email" required>
             <label for="floatingPassword">Email</label>
           </div>
           <div class="form-floating mb-3">
-            <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password" minlength="8" required>
+            <input name="password" type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password" minlength="8" required>
             <label for="floatingPassword">Password</label>
           </div>
           <div class="form-floating mb-3">
-            <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password" minlength="8" required>
+            <input name="password2" type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password" minlength="8" required>
             <label for="floatingPassword">Confirm Password</label>
           </div>
-          <a role="button" href="admin-panel/dashboard.php" class="w-100 mb-2 btn btn-lg rounded-3 btn-outline-danger" type="submit">Create account</a>
+          <input name="register" role="button" href="admin-panel/dashboard.php" class="w-100 mb-2 btn btn-lg rounded-3 btn-outline-danger" type="submit" value="Create account">
         </form> 
       </div>
     </div>
