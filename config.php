d76855e30ed2c793da37ad6f0a5647aea286d83a
Prox-C <amadeusgravoso@gmail.com>
2025-03-06 16:42:53 +0800

    added database connection, user registration and user data retrieval

---
+++ config.php
@@ -0,0 +1,88 @@
+<?php
+
+function connect() // DATABASE CONNECTION
+{
+    $host = "localhost";
+    $db = "redline_db";
+    $user = "root";
+    $password = "";
+    $dsn = "mysql:host=$host;dbname=$db";
+
+    try
+    {
+        $conn = new PDO($dsn, $user, $password);
+        return $conn;
+    }
+    catch(PDOException $e) 
+    {
+        echo "Error: " . $e->getMessage();
+		die();
+    }
+}
+
+// // USER-RELATED FUNCTIONS
+// function auth($email)
+// {
+//     $conn = connect();
+
+//     $query = $conn->prepare("SELECT * FROM `users` WHERE email = :email LIMIT 1");
+//     $query->bindParam(':email', $email);
+//     $query->execute();
+// 	$result = $query->fetch(PDO::FETCH_ASSOC);
+
+// 	return $result;
+// }
+
+function register_user($role, $email,$password, $fname, $lname, $birthdate, $sex, $pfp)
+{
+    $conn = connect();
+
+    // Check if the email already exists
+    $check_query = $conn->prepare("SELECT COUNT(*) FROM `users` WHERE email = :email");
+    $check_query->bindParam(':email', $email);
+    $check_query->execute();
+    $username_exists = $check_query->fetchColumn();
+
+    if ($username_exists) {
+        $conn = null;
+        return "email_exists";
+    }
+
+    // Hash the password before storing it
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $query = $conn->prepare("INSERT INTO `users` (role, email, password, fname, lname, birthdate, sex, profile_pic) VALUES (:role, :email, :password, :fname, :lname, :birthdate, :sex, :profile_pic)");
+
+    $query->bindParam(':role', $role);
+    $query->bindParam(':email', $email);
+    $query->bindParam(':password', $hashed_password);
+    $query->bindParam(':fname', $fname);
+    $query->bindParam(':lname', $lname);
+    $query->bindParam(':birthdate', $birthdate);
+    $query->bindParam(':sex', $sex);
+    $query->bindParam(':profile_pic', $pfp);
+
+    $response = $query->execute();
+
+    if ($response) {
+        $id = $conn->lastInsertId();
+        $conn = null;
+        return $id;
+    } else {
+        $conn = null;
+        return FALSE;
+    }
+}
+
+function fetch_users(){
+    $conn = connect();
+
+    $query = $conn->prepare("SELECT user_id, email, profile_pic, fname, lname, birthdate, sex  FROM `users` WHERE role = 3");
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $conn = null;
+    return $result;
+}
+
+?>
