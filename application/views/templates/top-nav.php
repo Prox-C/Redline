90ad939d1b01e7a44bc0fc944d905846398d4e53
Prox-C <amadeusgravoso@gmail.com>
2025-03-27 01:21:30 +0800

    better topnav for browse page

---
+++ application/views/templates/top-nav.php
@@ -0,0 +1,21 @@
+
+    <header class="d-flex flex-wrap justify-content-between py-3 mb-4 border-bottom bg-primary px-4">
+        <a href="" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <h3 style="font-family: 'NikkyouSans';" class="text-white fw-normal">レッドライン</h3>
+        </a>
+
+        <div class="dropdown text-end d-none d-flex align-items-center"> 
+            <a href="#" class="d-block link-body-emphasis text-decoration-none" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+            </a>
+            <ul class="dropdown-menu text-small">
+                <li><a class="dropdown-item" href="#">New project...</a></li>
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Sign out</a></li>
+            </ul>
+            <!-- <button type="button" class="btn btn-dark rounded-4 px-4" data-bs-toggle="modal" data-bs-target="#loginModal">Sign in</button> -->
+        </div>
+    
+    </header>
