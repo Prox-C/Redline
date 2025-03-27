603f0afebfd5c49742a48c904f7eb19deb74293a
Prox-C <amadeusgravoso@gmail.com>
2025-03-27 10:27:55 +0800

    fixed css conflicts

--- application/views/templates/top-nav.php
+++ application/views/templates/top-nav.php
@@ -4,18 +4,20 @@
             <h3 style="font-family: 'NikkyouSans';" class="text-white fw-normal">レッドライン</h3>
         </a>
 
-        <div class="dropdown text-end d-none d-flex align-items-center"> 
+        <div class="dropdown text-end d-flex align-items-center"> 
             <a href="#" class="d-block link-body-emphasis text-decoration-none" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                <img src="<?=base_url('/assets/images/users/profile.png')?>" alt="mdo" width="32" height="32" class="rounded-circle bg-dark border border-2 border-white">
             </a>
             <ul class="dropdown-menu text-small">
-                <li><a class="dropdown-item" href="#">New project...</a></li>
-                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><a class="dropdown-item" href="#">Notifications</a></li>
                 <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li><a class="dropdown-item" href="#">Settings</a></li> 
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Sign out</a></li>
+                <li><a class="dropdown-item text-danger" href="#">Sign out</a></li>
             </ul>
             <!-- <button type="button" class="btn btn-dark rounded-4 px-4" data-bs-toggle="modal" data-bs-target="#loginModal">Sign in</button> -->
         </div>
+
+        
     
     </header>
