136746c70dfa0dcaa20fa8693f01981157b12e60
Prox-C <amadeusgravoso@gmail.com>
2025-03-29 11:45:48 +0800

    ported reviews

---
+++ application/views/templates/reviews.php
@@ -0,0 +1,88 @@
+             <!-- /.card-body -->
+             <div class="card-footer card-comments">
+                <div class="card-comment">
+                  <!-- User image -->
+                  <img class="img-circle img-sm" src="<?= base_url('assets/images/users/ppg1.jpeg') ?>" alt="User Image">
+                  
+                  <div class="comment-text">
+                    <span class="username">
+                      Violet Myers
+                      <span class="text-muted float-right">8:03 PM Today</span>
+                    </span><!-- /.username -->
+                    It is a long established fact that a reader will be distracted
+                    by the readable content of a page when looking at its layout.
+                  </div>
+                  <!-- /.comment-text -->
+                </div>
+                <!-- /.card-comment -->
+                <div class="card-comment">
+                  <!-- User image -->
+                  <img class="img-circle img-sm" src="<?= base_url('assets/images/users/ppg2.jpeg') ?>" alt="User Image">
+
+                  <div class="comment-text">
+                    <span class="username">
+                      Jenifer Gordonivich
+                      <span class="text-muted float-right">8:03 PM Today</span>
+                    </span><!-- /.username -->
+                    The point of using Lorem Ipsum is that it hrs a morer-less
+                    normal distribution of letters, as opposed to using
+                    'Content here, content here', making it look like readable English.
+                  </div>
+                  <!-- /.comment-text -->
+                </div>
+                <!-- /.card-comment -->
+                                 <!-- /.card-comment -->
+                <div class="card-comment">
+                  <!-- User image -->
+                  <img class="img-circle img-sm" src="<?= base_url('assets/images/users/ppg3.jpeg') ?>" alt="User Image">
+
+                  <div class="comment-text">
+                    <span class="username">
+                      Eimi Fukada 
+                      <span class="text-muted float-right">8:03 PM Today</span>
+                    </span><!-- /.username -->
+                    The point of using Lorem Ipsum is that it hrs a morer-less
+                    normal distribution of letters, as opposed to using
+                    'Content here, content here', making it look like readable English.
+                  </div>
+                  <!-- /.comment-text -->
+                </div>
+                <!-- /.card-comment -->
+                                 <!-- /.card-comment -->
+                <div class="card-comment">
+                  <!-- User image -->
+                  <img class="img-circle img-sm" src="<?= base_url('assets/images/users/ploy.jpeg') ?>" alt="User Image">
+
+                  <div class="comment-text">
+                    <span class="username">
+                      John Ploy
+                      <span class="text-muted float-right">8:03 PM Today</span>
+                    </span><!-- /.username -->
+                    The point of using Lorem Ipsum is that it hrs a morer-less
+                    normal distribution of letters, as opposed to using
+                    'Content here, content here', making it look like readable English.
+                  </div>
+                  <!-- /.comment-text -->
+                </div>
+                <!-- /.card-comment -->
+                                 <!-- /.card-comment -->
+                <div class="card-comment">
+                  <!-- User image -->
+                  <img class="img-circle img-sm" src="<?= base_url('assets/images/users/bypass.jpeg') ?>" alt="User Image">
+
+                  <div class="comment-text">
+                    <span class="username">
+                      Sta. Cruz Street
+                      <span class="text-muted float-right">8:03 PM Today</span>
+                    </span><!-- /.username -->
+                    The point of using Lorem Ipsum is that it hrs a morer-less
+                    normal distribution of letters, as opposed to using
+                    'Content here, content here', making it look like readable English.
+                  </div>
+                  <!-- /.comment-text -->
+                </div>
+                <!-- /.card-comment -->
+                 
+              </div>
+            </div>
+            <!-- /.card -->
