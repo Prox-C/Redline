a900fb9cc6267f812b4b5f6f543107c3085d6c3d
Prox-C <amadeusgravoso@gmail.com>
2025-04-04 15:04:56 +0800

    car add method fixed

--- application/models/CarModel.php
+++ application/models/CarModel.php
@@ -24,27 +24,28 @@ class CarModel extends CI_Model {
 
     function addCar($imageFileName) {
         $data = array(
-            'thumbnail' => $imageFileName,
-            'brand' => $this->input->post('brand-select'),
-            'model' => $this->input->post('model'),
-            'description' => $this->input->post('description'),
-            'power' => $this->input->post('power'),
-            'speed' => $this->input->post('speed'),
-            'capacity' => $this->input->post('capacity'),
+            'thumbnail'    => $imageFileName,
+            'brand'        => $this->input->post('brand-select'),
+            'model'        => $this->input->post('model'),
+            'description'  => $this->input->post('description'),
+            'power'        => $this->input->post('power'),
+            'speed'        => $this->input->post('speed'),
+            'capacity'     => $this->input->post('capacity'),
             'transmission' => $this->input->post('transmission'),
-            'fuel' => $this->input->post('fuel'),
-            'rate' => $this->input->post('rate'),
-            'rating' => 5.0,
-            'status' => 0
+            'fuel'         => $this->input->post('fuel'),
+            'rate'         => $this->input->post('rate'),
+            'rating'       => 5.0,
+            'status'       => 0
         );
         
         if ($this->db->insert('cars', $data)) {
-            $this->session->set_flashdata('success', 'Car added!');
+            $this->session->set_flashdata('success', 'Car added successfully!');
         } else {
             $this->session->set_flashdata('error', 'Failed to add car.');
         }
         redirect('admin/cars');
     }
+    
     
     
 }
