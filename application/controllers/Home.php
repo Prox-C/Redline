a7b26dad3ca41b9e72b2c6e91c9c2bc7eb8f95f6
Amadeus Gravoso <amadeusgravoso@gmail.com>
2025-04-07 17:05:45 +0800

    added client self registration and validaton

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,4 +9,27 @@ class Home extends CI_Controller {
     public function browse() {
         $this->load->view('pages/browse');
     }
+
+    public function register() {
+        if ($this->input->server('REQUEST_METHOD') === 'POST') {
+            $this->load->library('form_validation');
+    
+            // Form validation rules
+            $this->form_validation->set_rules('fname', 'First Name', 'required');
+            $this->form_validation->set_rules('lname', 'Last Name', 'required');
+            $this->form_validation->set_rules('bday', 'Birthday', 'required');
+            $this->form_validation->set_rules('sex', 'Sex', 'required');
+            $this->form_validation->set_rules('email', 'Email', 'required');
+            $this->form_validation->set_rules('password', 'Password', 'required|min_length[8]');
+            $this->form_validation->set_rules('password2', 'Confirm Password', 'required|min_length[8]');
+    
+            if ($this->form_validation->run() == FALSE) {
+                // Set error flag for the registration form
+                $this->session->set_flashdata('validation-error', 'registerModal');
+                $this->load->view('index');
+            } else {
+                $this->UserModel->selfRegister();
+            }
+        }
+    }
 }
