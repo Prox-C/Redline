30d62edb45816bc42b6ab9c1b5a17b703d8905bd
Amadeus Gravoso <amadeusgravoso@gmail.com>
2025-04-07 23:04:07 +0800

    added swal2

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -14,6 +14,9 @@
     <link rel="stylesheet" href="<?= base_url('assets/dist/css/adminlte.min.css?v=3.2.0') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css') ?>"> 
 
+    <!-- SweetAlert2 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
     <!-- FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -406,6 +409,31 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url('assets/dist/js/adminlte.min.js?v=3.2.0') ?>"></script>
+<!-- SweetAlert2 JS -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
+
+<?php if ($this->session->flashdata('login-success')): ?>
+  <script>
+    var Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 5000,
+      customClass: {
+        popup: 'custom-toast'
+      }
+    });
+
+    Toast.fire({
+      html: `
+        <div style="display: flex; align-items: center;">
+          <img src="https://media.tenor.com/SNL9_xhZl9oAAAAj/waving-hand-joypixels.gif" width="30" height="30" style="margin-right: 10px;">
+          <span><?= $this->session->flashdata('login-success');?></span>
+        </div>
+      `
+    });
+  </script>
+<?php endif; ?>
 
 </html>
 
