6c60f355b648a643a7557e61a5762ac116d1d0aa
Amadeus Gravoso <amadeusgravoso@gmail.com>
2025-04-07 17:07:53 +0800

    added modal for when email already registered

--- application/views/templates/forms.php
+++ application/views/templates/forms.php
@@ -17,7 +17,7 @@
             <label for="floatingPassword">Password</label>
           </div>
           <a role="button" href="admin/home" class="w-100 mb-2 btn btn-lg rounded-4 btn-bd-primary" type="submit">Login</a>
-          <small class="text-body-secondary text-center d-block">
+          <small class="text-body-secondary text-center d-block mt-2">
             Don't have an account? 
             <a class="txt-pri" data-bs-target="#registerModal" data-bs-toggle="modal" data-bs-dismiss="modal" role="button">Sign up</a>
           </small>
@@ -36,57 +36,64 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body p-5 pt-0">
-        <form>
-          <label class="form-label">Personal Information</label>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-floating mb-3">
-                <input type="email" class="form-control rounded-3" placeholder="First name" required>
-                <label>First name</label>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-floating mb-3">
-                <input type="email" class="form-control rounded-3" placeholder="name@example.com" required>
-                <label>Last name</label>
-              </div>
+      <?= form_open('register'); ?>
+        <label class="form-label">Personal Information</label>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-floating mb-3">
+              <input name="fname" type="text" class="form-control rounded-3" placeholder="First name" value="<?= set_value('fname'); ?>">
+              <label>First name</label>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-floating mb-3">
-                <input type="date" class="form-control rounded-3" id="pickupDate" name="rentStart" required>
-                <label for="pickupDate" class="text-muted-subtle">Date of Birth</label>
-              </div>
-            </div>
-            <div class="col-md-6 pb-3">
-              <select class="form-select form-select-md mb-3 rounded-3 h-100 m-0" aria-label=".form-select-lg example">
-                <option class="d-none" selected>Sex<span class="text-muted-subtle"></span></option>
-                <option value="1">Male</option>
-                <option value="2">Female</option>
-                <option value="3">Prefer not to say</option>
-              </select>
+          <div class="col-md-6">
+            <div class="form-floating mb-3">
+              <input name="lname" type="text" class="form-control rounded-3" placeholder="Last name" value="<?= set_value('lname'); ?>">
+              <label>Last name</label>
             </div>
           </div>
-          <label for="" class="form-label">Contact Details</label>
-          <div class="form-floating mb-3">
-            <input type="email" class="form-control rounded-3" placeholder="Email" required>
-            <label for="floatingPassword">Email</label>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-floating mb-3">
+              <input name="bday" type="date" class="form-control rounded-3" id="pickupDate" value="<?= set_value('bday'); ?>">
+              <label for="pickupDate" class="text-muted-subtle">Date of Birth</label>
+            </div>
           </div>
-          <div class="form-floating mb-3">
-            <input type="password" class="form-control rounded-3" placeholder="Password" minlength="8" required>
-            <label for="floatingPassword">Password</label>
+          <div class="col-md-6 pb-3">
+            <select name="sex" class="form-select form-select-md mb-3 rounded-3 h-100 m-0" aria-label=".form-select-lg example">
+              <option class="d-none" value="" <?= set_select('sex', '', TRUE); ?>>Sex</option>
+              <option value="1" <?= set_select('sex', '1'); ?>>Male</option>
+              <option value="2" <?= set_select('sex', '2'); ?>>Female</option>
+              <option value="3" <?= set_select('sex', '3'); ?>>Prefer not to say</option>
+            </select>
           </div>
-          <!-- <div class="form-floating mb-3">
-            <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password" minlength="8" required>
-            <label for="floatingPassword">Confirm Password</label>
-          </div> -->
-          <a role="button" href="admin-panel/dashboard.php" class="w-100 mb-2 btn btn-lg rounded-4 btn-bd-primary" type="submit">Register</a>
-          <small class="text-body-secondary text-center d-block">
-            Already have an account? 
-            <a class="txt-pri" data-bs-target="#loginModal" data-bs-toggle="modal" data-bs-dismiss="modal" role="button">Sign in</a>
-          </small>
-        </form> 
+        </div>
+        <label for="" class="form-label">Contact Details</label>
+        <div class="form-floating mb-3">
+          <input name="email" type="email" class="form-control rounded-3" placeholder="Email" value="<?= set_value('email'); ?>">
+          <label for="floatingPassword">Email</label>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-floating mb-3">
+              <input name="password" type="password" class="form-control rounded-3" placeholder="Password">
+              <label>Password</label>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-floating mb-3">
+              <input name="password2" type="password" class="form-control rounded-3" placeholder="Confirm Password">
+              <label>Confirm Password</label>
+            </div>
+          </div>
+        </div>
+        <button class="w-100 mb-2 btn btn-lg rounded-4 btn-bd-primary" type="submit">Register</button>
+        <small class="text-body-secondary text-center d-block mt-2">
+          Already have an account? 
+          <a class="txt-pri" data-bs-target="#loginModal" data-bs-toggle="modal" data-bs-dismiss="modal" role="button">Sign in</a>
+        </small>
+      <?= form_close(); ?>
+
       </div>
     </div>
   </div>
@@ -148,6 +155,22 @@
           <input name="register" role="button" class="w-100 mb-2 btn btn-lg rounded-4 btn-danger" type="submit" value="Create account">
       <?= form_close(); ?>
 
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Email Already Exists -->
+<div class="modal fade modal-sheet" tabindex="-1" aria-hidden="true"role="dialog" id="emailErrorModal">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content rounded-4 shadow">
+      <div class="modal-body p-4 text-center">
+        <h5 class="mb-2">Email already in-use.</h5>
+        <p class="mb-0">This email is already linked to an account.<br>Would you like to sign in instead?</p>
+      </div>
+      <div class="modal-footer flex-nowrap p-0">
+        <button type="button" class="btn btn-lg btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0 text-muted border-end" data-bs-dismiss="modal">Cancel</button>
+        <a role="button" class="d-flex align-tems-center justify-content-center fs-6 text-decoration-none col-6 py-3 m-0 rounded-0 text-danger" data-bs-target="#loginModal" data-bs-toggle="modal">Yes</a>
       </div>
     </div>
   </div>
