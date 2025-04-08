6a771fb1d334805797adf2e0eeeab0dc4906777c
Prox-C <amadeusgravoso@gmail.com>
2025-04-09 01:54:19 +0800

    removed booking management method

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -2,10 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Book extends CI_Controller {
-    public function getBookings() {
-        $this->load->view('admin/bookings');
-    }
-
     public function checkout() {
         $this->load->view('pages/checkout');
     }
