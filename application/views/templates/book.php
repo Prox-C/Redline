589d806d848b226d860b6bdf17b8cb2888ee8336
Prox-C <amadeusgravoso@gmail.com>
2025-04-12 23:59:38 +0800

    finalized date selection form

--- application/views/templates/book.php
+++ application/views/templates/book.php
@@ -2,12 +2,16 @@
 <div class="modal fade" tabindex="-1" id="bookModal" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content rounded-4 shadow">
-      <div class="modal-header p-5 pb-4 border-bottom-0">
-        <h4 class="fw-bold mb-0 text-center">Pick Your Dates</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="modal-header p-5 pb-2 border-bottom-0">
+        <div class="col-12">
+          <h4 class="fw-bold mb-0 mb-3">Pick Your Dates</h4>
+          <p class="text-muted">
+            Confirm your rental period so we can reserve it just for you.
+          </p>
+        </div>
       </div>
       <div class="modal-body p-5 pt-0">
-        <?= form_open('login'); ?>
+        <?= form_open(''); ?>
           <div class="form-floating position-relative mb-3">
             <input type="text" class="form-control rounded-3 pe-5" id="pickupDate" name="rentStart" placeholder="Pickup" required>
             <label for="pickupDate" class="text-muted-subtle fw-normal">Pickup</label>
@@ -22,7 +26,14 @@
               <i class="bi bi-calendar"></i>
             </span>
           </div>
-          <button class="w-100 btn btn-lg rounded-4 btn-bd-primary" type="submit">Login</button>
+          <div class="row gap-0 px-2">
+            <div class="col-md-6 g-2">
+              <button type="button" data-bs-dismiss="modal" class="w-100 btn rounded-4 bg-secondary-subtle text-dark">Cancel</button>
+            </div>
+            <div class="col-md-6 g-2">
+              <button class="w-100 btn rounded-4 btn-bd-primary" type="">Confirm</button>
+            </div>
+          </div>
         <?= form_close(); ?>
       </div>
     </div>
