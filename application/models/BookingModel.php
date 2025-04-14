01bfa18280824510f186467a1d6f589143daa735
Prox-C <amadeusgravoso@gmail.com>
2025-04-14 14:47:49 +0800

    initialized booking model

--- application/models/BookingModel.php
+++ application/models/BookingModel.php
@@ -4,32 +4,5 @@ class BookingModel extends CI_Model {
         parent::__construct(); 
         $this->load->database(); 
     }
-
-    public function searchAvailableCars($search = '', $start = '', $end = '') {
-        $this->db->select('*');
-        $this->db->from('cars');
-    
-        if (!empty($search)) {
-            $this->db->group_start();
-            $this->db->like('brand', $search);
-            $this->db->or_like('model', $search);
-            $this->db->group_end();
-        }
-    
-        if (!empty($start) && !empty($end)) {
-            $this->db->where("id NOT IN (
-                SELECT car_id FROM bookings 
-                WHERE status IN ('pending', 'confirmed') 
-                AND (
-                    (pickup_date <= '$end' AND dropoff_date >= '$start')
-                )
-            )");
-        }
-    
-        return $this->db->get()->result_array();
-    }
-    
-    
- 
 }
 ?>
