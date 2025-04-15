d0fcd1c7d351743da7060b558722a7a69ca03db1
Prox-C <amadeusgravoso@gmail.com>
2025-04-15 12:57:27 +0800

    added logic for filtering out unavailable dates

--- application/models/BookingModel.php
+++ application/models/BookingModel.php
@@ -4,5 +4,24 @@ class BookingModel extends CI_Model {
         parent::__construct(); 
         $this->load->database(); 
     }
+
+    public function getUnavailableDates($carId) {
+        $this->db->where('car_id', $carId);
+        $query = $this->db->get('bookings');
+    
+        $unavailable = [];
+        foreach ($query->result() as $booking) {
+            $start = new DateTime($booking->pickup_date);
+            $end = new DateTime($booking->dropoff_date);
+            $interval = new DateInterval('P1D');
+            $range = new DatePeriod($start, $interval, $end->modify('+1 day'));
+    
+            foreach ($range as $date) {
+                $unavailable[] = $date->format('Y-m-d');
+            }
+        }
+    
+        return $unavailable;
+    }
 }
 ?>
