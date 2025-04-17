763329d6769761a6ad944b232821f4cdd6324160
Prox-C <amadeusgravoso@gmail.com>
2025-04-17 16:11:42 +0800

    done fixed booking form handling

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -42,8 +42,42 @@ class Book extends MY_Secured {
         
         $data['rent_duration'] = $days;
         $data['car'] = $this->CarModel->getCarByID($car_id);
-
         $this->load->view('pages/checkout', $data);
     }
 
+    public function index() {
+        // Load form helper and validation library
+        $this->load->library('form_validation');
+    
+        // Set form validation rules
+        $this->form_validation->set_rules('fname', 'First Name', 'required');
+        $this->form_validation->set_rules('lname', 'Last Name', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+        $this->form_validation->set_rules('address', 'Address', 'required');
+        $this->form_validation->set_rules('paymentMethod', 'Payment Method', 'required');
+        $this->form_validation->set_rules('card_name', 'Name on Card');
+        $this->form_validation->set_rules('card_no', 'Card Number', 'max_length[20]');
+        $this->form_validation->set_rules('card_expiration', 'Expiration Date');
+        $this->form_validation->set_rules('card_cvv', 'CVV', 'numeric|exact_length[3]');
+        
+    
+        if ($this->form_validation->run() == FALSE) {
+            // Form has errors - reload booking page
+            $pickup = new DateTime($this->input->post('rentStart'));
+            $dropoff = new DateTime($this->input->post('rentEnd'));
+            $days = $pickup->diff($dropoff)->days;
+            if ($days == 0) $days = 1;
+    
+            $data['rent_duration'] = $days;
+            $car_id = $this->input->post('car_id');
+            $data['car'] = $this->CarModel->getCarByID($car_id);
+            $this->session->flashdata('validation-error', 'Please fill out all required fields.');
+            $this->load->view('pages/checkout', $data);
+        } else {
+            // Call model method to insert booking
+            $this->load->model('BookingModel');
+            $this->BookingModel->addBooking();
+        }
+    }
+    
 }
