12097eb74f30cc72e15428b81ac76433afe65e41
Amadeus Gravoso <amadeusgravoso@gmail.com>
2025-04-24 03:39:57 +0800

    reformatted

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -11,40 +11,6 @@ class Book extends MY_Secured {
         $this->load->model('BookingModel');
     }
 
-    public function checkout() {
-
-        $this->load->library('form_validation');
-
-        $this->form_validation->set_rules('rentStart', 'Pickup Date', 'required');
-        $this->form_validation->set_rules('rentEnd', 'Dropoff Date', 'required');
-
-        if ($this->form_validation->run() == FALSE) {
-
-            $this->session->set_flashdata('validation-error', 'bookModal');
-            redirect($_SERVER['HTTP_REFERER']); // redirect back to form page
-            exit; // <-- Force it to stop executing and ensure session saves
-
-        }
-        // Get the form data
-        $pickup = $this->input->post('rentStart'); 
-        $dropoff = $this->input->post('rentEnd');
-        $car_id = $this->input->post('book_car_id');
-
-        // Optional: Save to session or use directly
-        $this->session->set_userdata('pickup-date', $pickup);
-        $this->session->set_userdata('dropoff-date', $dropoff);
-
-        $pickup = new DateTime($this->input->post('rentStart'));
-        $dropoff = new DateTime($this->input->post('rentEnd'));
-        $days = $pickup->diff($dropoff)->days;
-        if ($days == 0) $days = 1;
-
-        
-        $data['rent_duration'] = $days;
-        $data['car'] = $this->CarModel->getCarByID($car_id);
-        $this->load->view('pages/checkout', $data);
-    }
-
     public function index() {
         // Load form helper and validation library
         $this->load->library('form_validation');
@@ -80,5 +46,46 @@ class Book extends MY_Secured {
             $this->BookingModel->addBooking();
         }
     }
+
+    public function checkout() {
+
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('rentStart', 'Pickup Date', 'required');
+        $this->form_validation->set_rules('rentEnd', 'Dropoff Date', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+
+            $this->session->set_flashdata('validation-error', 'bookModal');
+            redirect($_SERVER['HTTP_REFERER']); // redirect back to form page
+            exit; // <-- Force it to stop executing and ensure session saves
+
+        }
+        // Get the form data
+        $pickup = $this->input->post('rentStart'); 
+        $dropoff = $this->input->post('rentEnd');
+        $car_id = $this->input->post('book_car_id');
+
+        // Optional: Save to session or use directly
+        $this->session->set_userdata('pickup-date', $pickup);
+        $this->session->set_userdata('dropoff-date', $dropoff);
+
+        $pickup = new DateTime($this->input->post('rentStart'));
+        $dropoff = new DateTime($this->input->post('rentEnd'));
+        $days = $pickup->diff($dropoff)->days;
+        if ($days == 0) $days = 1;
+
+        
+        $data['rent_duration'] = $days;
+        $data['car'] = $this->CarModel->getCarByID($car_id);
+        $this->load->view('pages/checkout', $data);
+    }
+
+    public function getClientBookings($user_id) {
+        $data['bookings'] = $this->BookingModel->getClientBookings($user_id);
+        $this->load->view('pages/bookings', $data);
+    }
+
+
     
 }
