50176d2bbba1e0ee8540878debfbd7452387fe99
Amadeus Gravoso <amadeusgravoso@gmail.com>
2025-04-26 21:27:00 +0800

    adjusted links

--- application/views/templates/fab.php
+++ application/views/templates/fab.php
@@ -14,7 +14,7 @@
           <path d="M240,102c0,70-103.79,126.66-108.21,129a8,8,0,0,1-7.58,0C119.79,228.66,16,172,16,102A62.07,62.07,0,0,1,78,40c20.65,0,38.73,8.88,50,23.89C139.27,48.88,157.35,40,178,40A62.07,62.07,0,0,1,240,102Z"></path>
         </svg>
       </button>
-      <button type="button" class="btn btn-sub btn-bd-primary" data-bs-toggle="tooltip" data-bs-placement="left" title="Bookings">
+      <button type="button" class="btn btn-sub btn-bd-primary" data-bs-toggle="tooltip" data-bs-placement="left" title="Bookings" onclick="window.location='<?= base_url('bookings/'.$this->session->userdata('user_id')) ?>'">
         <svg class="fab-icon" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 256 256">
           <path d="M240,104H229.2L201.42,41.5A16,16,0,0,0,186.8,32H69.2a16,16,0,0,0-14.62,9.5L26.8,104H16a8,8,0,0,0,0,16h8v80a16,16,0,0,0,16,16H64a16,16,0,0,0,16-16v-8h96v8a16,16,0,0,0,16,16h24a16,16,0,0,0,16-16V120h8a8,8,0,0,0,0-16ZM80,152H56a8,8,0,0,1,0-16H80a8,8,0,0,1,0,16Zm120,0H176a8,8,0,0,1,0-16h24a8,8,0,0,1,0,16ZM44.31,104,69.2,48H186.8l24.89,56Z"></path>
         </svg>
