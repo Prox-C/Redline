e8100f2362af1a3c0a2172d13bca4b343d080795
Prox-C <amadeusgravoso@gmail.com>
2025-04-27 14:00:42 +0800

    more conditional rendering +  minor ui tweaks

--- application/views/pages/view.php
+++ application/views/pages/view.php
@@ -257,11 +257,30 @@
                 </div>
 
                   <div class="col-lg-8">
-                    <?php if($this->session->userdata('logged_in')){?>
-                    <button class="btn btn-lg btn-bd-primary w-100 rounded-4" data-bs-toggle="modal" data-bs-target="#bookModal">Book now</button>
-                    <?php } else { ?>
-                    <button type="button" class="btn btn-lg bg-dark-subtle fw-medium border-0 w-100 rounded-4" disabled>Sign in to book</button>
-                    <?php } ?>
+                  <?php if ($this->session->userdata('logged_in')): ?>
+                  <?php if (isset($car['status']) && $car['status'] === 'Maintenance'): ?>
+                      <button type="button"
+                              class="btn btn-lg bg-dark-subtle fw-normal border-0 w-100 rounded-4"
+                              disabled>
+                          Under Maintenance
+                      </button>
+                  <?php else: ?>
+                      <button type="button"
+                              class="btn btn-lg btn-bd-primary w-100 rounded-4"
+                              data-bs-toggle="modal"
+                              data-bs-target="#bookModal">
+                          Book now
+                          <svg xmlns="http://www.w3.org/2000/svg" width="22" style="position: relative; bottom: 1px" fill="currentColor" viewBox="0 0 256 256"><path d="M224.49,136.49l-72,72a12,12,0,0,1-17-17L187,140H40a12,12,0,0,1,0-24H187L135.51,64.48a12,12,0,0,1,17-17l72,72A12,12,0,0,1,224.49,136.49Z"></path></svg>
+                      </button>
+                  <?php endif; ?>
+                  <?php else: ?>
+                  <button type="button"
+                          class="btn btn-lg bg-dark-subtle fw-medium border-0 w-100 rounded-4"
+                          disabled>
+                      Sign in to book
+                  </button>
+                  <?php endif; ?>
+
                   </div>
                 </div>
             </div>
