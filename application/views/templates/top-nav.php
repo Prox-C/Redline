d83d761a90e846f4f68a15aef6992758463e0543
Amadeus Gravoso <amadeusgravoso@gmail.com>
2025-05-08 04:03:46 +0800

    fixed redirection on liked link

--- application/views/templates/top-nav.php
+++ application/views/templates/top-nav.php
@@ -17,11 +17,11 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="nav-icon me-1" fill="currentColor" viewBox="0 0 256 256"><path d="M240,104H229.2L201.42,41.5A16,16,0,0,0,186.8,32H69.2a16,16,0,0,0-14.62,9.5L26.8,104H16a8,8,0,0,0,0,16h8v80a16,16,0,0,0,16,16H64a16,16,0,0,0,16-16v-8h96v8a16,16,0,0,0,16,16h24a16,16,0,0,0,16-16V120h8a8,8,0,0,0,0-16ZM80,152H56a8,8,0,0,1,0-16H80a8,8,0,0,1,0,16Zm120,0H176a8,8,0,0,1,0-16h24a8,8,0,0,1,0,16ZM44.31,104,69.2,48H186.8l24.89,56Z"></path></svg>
                     Bookings
                 </a>
-                <a href="<?= base_url('admin/history') ?>" class="nav-link px-2">
+                <a href="<?= base_url('likes/'.$this->session->userdata('user_id')) ?>" class="nav-link px-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="nav-icon me-1" fill="currentColor" viewBox="0 0 256 256"><path d="M240,102c0,70-103.79,126.66-108.21,129a8,8,0,0,1-7.58,0C119.79,228.66,16,172,16,102A62.07,62.07,0,0,1,78,40c20.65,0,38.73,8.88,50,23.89C139.27,48.88,157.35,40,178,40A62.07,62.07,0,0,1,240,102Z"></path></svg>                    
                     Liked
                 </a>
-                <a href="<?= base_url('admin/notifications') ?>" class="nav-link px-2 align-middle">
+                <a href="" class="nav-link px-2 align-middle">
                 <svg xmlns="http://www.w3.org/2000/svg" class="nav-icon me-1" fill="currentColor" viewBox="0 0 256 256">
                     <path d="M221.8,175.94C216.25,166.38,208,139.33,208,104a80,80,0,1,0-160,0c0,35.34-8.26,62.38-13.81,71.94A16,16,0,0,0,48,200H88.81a40,40,0,0,0,78.38,0H208a16,16,0,0,0,13.8-24.06ZM128,216a24,24,0,0,1-22.62-16h45.24A24,24,0,0,1,128,216Z"></path>
                 </svg>
